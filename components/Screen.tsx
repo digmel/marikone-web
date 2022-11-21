5fcd9fc524ba88e6a26acd298645727f603b6197
George <gio.digmel@gmail.com>
2022-11-20 19:00:54 -0800

    fix: added sticky header

--- components/Screen.tsx
+++ components/Screen.tsx
@@ -5,11 +5,11 @@ import { Header } from "./Header";
 export const Screen = ({ children }: any) => {
   return (
     <div className=" bg-zinc-900 min-h-screen w-screen flex flex-col">
-      <div className="flex-initial w-screen">
+      <div className="flex-initial w-screen fixed">
         <Header />
       </div>
 
-      <div className="w-screen overflow-visible ">{children}</div>
+      <div className="w-screen overflow-visible mt-24 ">{children}</div>
 
       <div className="flex-initial w-screen">
         <Footer />
