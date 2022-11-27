56a238188d3cd390c5223d5c4c19b16568a2abb1
digmel <47932883+digmel@users.noreply.github.com>
2022-11-27 22:08:48 +0000

    Deploying to public from @ digmel/marikone-web@aa4cdc8e24e7b88a9efd5e2e88aa4e2c6ce2b78c 🚀

--- _next/static/b2q18Atc4HWyv-s9-ubsi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/675-d1dcbf5a9320f69a.js","static/chunks/pages/index-bc0fb7446a7e1108.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
