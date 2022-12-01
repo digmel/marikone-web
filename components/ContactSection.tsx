4c812ec12466f2ad5ae020ac086a6e51eb594457
George <gio.digmel@gmail.com>
2022-11-30 19:58:45 -0800

    chore: added contact form

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -8,8 +8,8 @@ export const ContactSection = () => {
     <div className="mb-64 flex flex-col items-center">
       <ContactSectionIcon />
 
-      <div className="bg-zinc-800 w-3/4 rounded-lg mt-12 flex flex-row py-12 px-14">
-        <div className="flex basis-1/4 border-r border-zinc-50 border-opacity-50 flex-col">
+      <div className="bg-zinc-800 w-fit rounded-lg mt-12 flex flex-row py-12 px-14">
+        <div className="flex border-r border-zinc-50 border-opacity-50 flex-col pr-12 -mt-2">
           <h1 className="text-accent-100 font-semibold">Store Locations</h1>
 
           <StoreInfo
@@ -29,15 +29,15 @@ export const ContactSection = () => {
             address=" 32 Zakaria Paliashvili St, T'bilisi"
           />
         </div>
-        <div className="flex basis-3/4 px-12 flex-col">
-          <div className="flex justify-around">
-            <TextInput label="First Name" />
-            <TextInput label="Last Name" />
-          </div>
 
-          {/* <div className="flex">
-            <TextInput label="Email" />
-          </div> */}
+        <div className="flex basis-3/4 flex-col items-center justify-center pl-12">
+          <TextInput label="Name" className="mb-8" />
+          <TextInput label="Email" className="mb-8" />
+          <TextInput label="Message" variant="textarea" className="mb-8" />
+
+          <button className="w-32 h-12 bg-accent-200 rounded self-start ml-4">
+            <h1 className="text-dark-200 text-lg">Submit</h1>
+          </button>
         </div>
       </div>
     </div>
--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
+
+type TVariant = "input" | "textarea";
 
 type TTextInputProps = {
   label: string;
@@ -6,6 +8,8 @@ type TTextInputProps = {
   isDisabled?: boolean;
   onFocus?: any;
   placeholder?: string;
+  variant?: TVariant;
+  className?: string;
 };
 
 export const TextInput = ({
@@ -13,20 +17,29 @@ export const TextInput = ({
   isDisabled = false,
   onFocus,
   placeholder,
+  variant = "input",
+  className,
 }: TTextInputProps) => {
   return (
     <>
-      <div className="">
+      <div className={className}>
         <div className="ml-4 px-2 absolute -mt-4">
           <h1 className=" bg-zinc-800 px-2 text-light ">{label}</h1>
         </div>
-        <input
-          className={`rounded-sm h-10 w-80 border border-light bg-transparent z-0 text-light px-4`}
-          disabled={isDisabled}
-          onChange={onFocus}
-          placeholder={placeholder}
-          autoCapitalize="none"
-        />
+
+        {variant === "input" ? (
+          <input
+            className={`rounded-sm h-10 w-80 border border-light bg-transparent z-0 text-light px-4`}
+            disabled={isDisabled}
+            onChange={onFocus}
+            placeholder={placeholder}
+            autoCapitalize="none"
+          />
+        ) : (
+          <textarea
+            className={`rounded-sm w-80 border border-light bg-transparent z-0 text-light px-4 h-32 py-4`}
+          />
+        )}
       </div>
     </>
   );
