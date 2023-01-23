57385660cf58371c081e9da5ae0b32b4620c32c9
George <gio.digmel@gmail.com>
2023-01-22 19:43:29 -0600

    chore: test camera app

---
+++ pages/test.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+const CameraTest = () => {
+  const [hasStream, setHaStream] = useState(false);
+
+  const getUserMedia = async () => {
+    try {
+      let video = document.getElementById("video");
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          facingMode: "environment",
+        },
+      });
+
+      // @ts-ignore
+      video.srcObject = stream;
+      // @ts-ignore
+      video.play();
+    } catch (err) {
+      console.log(`Error: ${err}`);
+    }
+  };
+
+  useEffect(() => {
+    getUserMedia();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hi there</h1>
+
+      <video id="video">Hello</video>
+    </div>
+  );
+};
+
+export default CameraTest;
